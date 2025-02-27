de51edae1aff301642538bf86fa8bac67ce2b1f3
matthman2019 <matthman2019@gmail.com>
2025-02-26 20:56:00 -0500

    new file:   Scripts/ActualGame.js 	modified:   Scripts/GameEngine.js

--- Scripts/GameEngine.js
+++ Scripts/GameEngine.js
@@ -4,13 +4,24 @@ const ctx = gameCanvas.getContext("2d");
 let gameWidth = window.innerWidth;
 let gameHeight = window.innerHeight;
 
+let FPS = 60;
+let bgColor = 'blue';
+
 gameCanvas.width = gameWidth;
 gameCanvas.height = gameHeight;
 
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+// noneFunction is a default value for callbacks (to prevent errors)
+const noneFunction = ()=>{};
 
-// base class for drawable objects. They all need x, y, a color, and a z.
+function widthPercentToPixels(percent) {
+    return gameCanvas.width * (percent / 100);
+}
+
+function heightPercentToPixels(percent) {
+    return gameCanvas.height * (percent / 100);
+}
+
+// base class for drawable objects. They all need x, y, a color, and a z. (Z is depth for sorting order)
 class drawObject {
     constructor(x, y, color='black', z='0') {
         this.x = x;
@@ -20,6 +31,7 @@ class drawObject {
     }
 }
 
+// rect is a rectangle class. Top-left position (x, y), width w, and height h.
 class rect extends drawObject{
     constructor(x, y, w, h, color='black', z='0') {
         super(x, y, color, z);
@@ -33,6 +45,7 @@ class rect extends drawObject{
     }
 }
 
+// circle is a drawnObject class for a circle. It has position (x, y) and a radius of r.
 class circle extends drawObject{
     constructor(x, y, r, color='black', z='0') {
         super(x, y, color, z)
@@ -43,22 +56,122 @@ class circle extends drawObject{
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.fill();
         ctx.stroke();
     }
 }
 
-class drawnSprite {
-    constructor(shape, updateCallback, startCallback) {
-        // run startCallback
-        this.shape = shape;
+// sprite is the class for things that run in-game. All scripting will be inside its callbacks.
+class sprite {
+    constructor(updateCallback=noneFunction, startCallback=noneFunction, updateAfterFrame=noneFunction) {
         this.update = updateCallback;
         this.start = startCallback;
+        this.afterFrame = updateAfterFrame;
 
         try{startCallback()} catch(error){console.error(error);}
     }
 }
 
-let e = new circle(10, 10, 5, 'black');
-e.draw();
+// drawnSprite is the class for sprites that are drawn.
+class drawnSprite extends sprite {
+    constructor(shape, updateCallback=noneFunction, startCallback=noneFunction, updateAfterFrame=noneFunction) {
+        super(updateCallback, startCallback, updateAfterFrame);
+        // run startCallback
+        this.shape = shape;
 
+        try{startCallback()} catch(error){console.error(error);}
+    }
+
+    draw() {
+        this.shape.draw();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// I have to do special things with runArray so I made it a class
+class runArrayClass {
+    constructor(items) {
+        this.items = items
+    }
+
+    // sort through items based on depth.
+    sortItems() {
+        // try sorting them first
+        try {
+            this.items.sort((a, b)=>{
+                return a.z - b.z;
+            });
+        } catch {
+            // if they don't have a z component, warn in the console and get rid of it.
+            console.error("There is something in runArray without a z component. Removing it now.");
+            // find the item lacking the 'z'
+            for (let i =0; i<this.items.length; i++) {
+                let thing = this.items[i];
+                if (!'z' in thing) {
+                    this.items.splice(i, 1);
+                    break;
+                }
+            }
+            // try sorting items again.
+            this.sortItems();
+        }
+    }
+        
+
+    push(thing) {
+        this.items.push(thing);
+    }
+}
+
+
+// everything that runs needs to be in runArray
+let runArray = new runArrayClass([]);
+
+
+// main loop
+function mainLoop() {
+    // sorting is done in the runArray object
+
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+    // run update code and render
+    for (thing of runArray.items) {
+        // if it's a draw object, run
+        if (thing instanceof drawnSprite) {
+            thing.update(thing);
+            thing.draw();
+            thing.afterFrame(thing);
+        }
+    }
+
+    
+
+    setTimeout(mainLoop, Math.round(1000 / FPS));
+}
+
+// here's a basic example. This creates a circle sprite that, every frame, increases it's x y and radius by 3 pixels.
+/*
+let c = new drawnSprite(new circle(10, 10, 10, 'red', 0),
+    (self)=>{
+    self.shape.x += 3;
+    self.shape.y += 3;
+    self.shape.r += 3;
+})
+
+runArray.push(c);
+*/
+
+//mainLoop();
 
